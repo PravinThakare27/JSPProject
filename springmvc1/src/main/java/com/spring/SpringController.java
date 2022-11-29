13ccfb061fa005ba8494ebba11dd296b9b750395
pravin.thakare <pthakare@27.com>
2022-11-29 23:08:25 +0530

    First commit

---
+++ springmvc1/src/main/java/com/spring/SpringController.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringController {
+@RequestMapping("/welcome")
+public ModelAndView welcome()
+{
+	return new ModelAndView("hello","msg","hello");
+}
+}
